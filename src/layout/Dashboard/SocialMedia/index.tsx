9b0a54c8fb030f2ce4de1d4edbc3083e6f0b03eb
Thales Marques <thalesf.galvao@gmail.com>
2026-07-06 17:01:31 -0300

    Feat: create social media icons

--- src/layout/Dashboard/SocialMedia/index.tsx
+++ src/layout/Dashboard/SocialMedia/index.tsx
@@ -1,10 +1,17 @@
 import * as S from "./style";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const SocialMedia = () => {
   return (
-    <S.Wrapper>
-      <p>SocialMedia</p>
-    </S.Wrapper>
+    <S.SocialMediaWrapper>
+      <S.SocialMediaItem>
+        <S.Link href="https://github.com/thalesfgalvao" target="_blank"><GitHubIcon />Github</S.Link>
+      </S.SocialMediaItem>
+      <S.SocialMediaItem>
+        <S.Link href="https://www.linkedin.com/in/thales-marques/" target="_blank"><LinkedInIcon />Linkedin</S.Link>
+      </S.SocialMediaItem>
+    </S.SocialMediaWrapper>
   );
 };
 
--- src/layout/Dashboard/SocialMedia/style.ts
+++ src/layout/Dashboard/SocialMedia/style.ts
@@ -1,5 +1,32 @@
 import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
+export const SocialMediaWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
     grid-row: 3/4;
+    gap: 2rem;
 `;
+
+export const SocialMediaItem = styled.div`
+    ${({ theme }) => css`
+    `}
+`;
+
+export const Link = styled.a`
+    ${({ theme }) => css`
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        font-size: ${theme.sizes.small};
+        color: ${theme.colors.body};
+        transition: 0.2s;
+        text-decoration: none;
+
+        &:hover {
+            color: ${theme.colors.primary};
+            transition: 0.2s;
+            cursor: pointer;
+            text-decoration: none;
+        }
+    `}
+`
