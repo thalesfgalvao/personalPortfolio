a4fd58ffcba495b276ab7978c093d94a8a818aa7
Thales Marques <thalesf.galvao@gmail.com>
2026-07-10 12:02:21 -0300

    Fix: change min-height value

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -17,11 +17,14 @@ export const GlobalStyle = createGlobalStyle`
   ${({ theme }) => css`
     html {
       font-size: 62.5%;
+      height: 100%;
     }
     body {
       font-family: "Inter", sans-serif;
       font-size: ${theme.sizes.medium};
       background-color: ${theme.colors.background};
+      width: 100%;
+      min-height: 100dvh;
     }
     ol,
     li {
