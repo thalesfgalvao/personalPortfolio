070ec5555cabdc4cd0355a7329734aeff8582180
Thales Marques <thalesf.galvao@gmail.com>
2026-07-15 01:25:04 -0300

    Feat: use media queries to set different svg sizes

--- src/layout/Dashboard/SocialMedia/style.ts
+++ src/layout/Dashboard/SocialMedia/style.ts
@@ -5,7 +5,8 @@ export const SocialMediaWrapper = styled.div`
     display: flex;
     flex-direction: column;
     grid-row: 3;
-    gap: 2rem;
+    justify-content: space-around;
+    gap: 1rem;
 
     @media ${theme.breakpoints.mobile} {
       display: flex;
@@ -34,11 +35,13 @@ export const Link = styled.a`
   ${({ theme }) => css`
     display: flex;
     flex-direction: row;
+    align-items: center;
     gap: 1rem;
     font-size: ${theme.sizes.normal};
     color: ${theme.colors.body};
     transition: 0.2s;
     text-decoration: none;
+    max-width: max-content;
 
     &:hover {
       color: ${theme.colors.primary};
@@ -46,11 +49,40 @@ export const Link = styled.a`
       cursor: pointer;
       text-decoration: none;
     }
+
+    @media ${theme.breakpoints.mobile} {
+      svg {
+        font-size: ${theme.sizes.medium};
+      }
+    }
+    @media ${theme.breakpoints.tablet} {
+      svg {
+        font-size: ${theme.sizes.large};
+      }
+    }
+    @media ${theme.breakpoints.laptop} {
+      margin-left: 0.5rem;
+      svg {
+        font-size: ${theme.sizes.large};
+      }
+    }
+    @media ${theme.breakpoints.desktop} {
+      margin-left: 0.5rem;
+      svg {
+        font-size: ${theme.sizes.large};
+      }
+    }
   `}
 `;
 
 export const LinkLabel = styled.label`
   ${({ theme }) => css`
+    &:hover {
+      color: ${theme.colors.primary};
+      transition: 0.2s;
+      cursor: pointer;
+      text-decoration: none;
+    }
     @media ${theme.breakpoints.mobile} {
       display: none;
       visibility: hidden;
